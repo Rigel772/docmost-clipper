920d7b925307d4f97cd7b496f05a405b9bfd7aaf
Emmanuel Legrand <eml.legrand@gmail.com>
2025-12-16 14:05:04 +0100

    Add icons and Cleanup: Removing Background Script

--- src/background.js
+++
@@ -1,5 +0,0 @@
-// src/background.js
-// Currently minimal, just a placeholder or for future expansion.
-chrome.runtime.onInstalled.addListener(() => {
-    console.log('Docmost Clipper installed');
-});
